18c3f39a43266f68da7c23d6def039f1e00b1713
Lucas Carioca <lucascarioca97@gmail.com>
2022-04-11 14:55:33 -0300

    adjustments to navbar routing when authenticated

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -20,51 +20,64 @@ const Navbar = () => {
 
     return (
         <Nav className="navbar navbar-expand-sm navbar-dark fixed-top">
-            <div className="container-fluid">
-                <img src={logo} alt="Generify Music Logo" style={{ width: '50px', height: 'auto', paddingRight: '10px' }} />
-                <LogoLink className="navbar-brand" to="/" ><b className="main-font">Generify</b></LogoLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <Toast className="loginAlert" onClose={() => setShowAlert(false)} variant="success" show={showAlert} delay={2000} autohide>
-                    <Toast.Body>{alertMsg}</Toast.Body>
-                </Toast>
-                {authenticated ? <div className="collapse navbar-collapse" id="collapsibleNavbar" >
-                    <ul className="navbar-nav ms-auto"  >
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/playlists" >
-                                Playlists
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <Button variant="clear" className="text-white" onClick={handleLogout}>Logout</Button>
-                        </li>
-                    </ul>
-                </div> : <div className="collapse navbar-collapse" id="collapsibleNavbar" >
-                    <ul className="navbar-nav ms-auto"  >
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/" >
-                                Início
-                            </NavLink>
-                        </li>
-                        <li className="nav-item" >
-                            <NavLink className="nav-link" to="/form" >
-                                Cadastro
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/login" >
-                                Login
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/faq" >
-                                FAQ
-                            </NavLink>
-                        </li>
-                    </ul>
+            {authenticated ?
+                <div className="container-fluid">
+                    <img src={logo} alt="Generify Music Logo" style={{ width: '50px', height: 'auto', paddingRight: '10px' }} />
+                    <LogoLink className="navbar-brand" to="/playlists" ><b className="main-font">Generify</b></LogoLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Toast className="loginAlert" onClose={() => setShowAlert(false)} variant="success" show={showAlert} delay={2000} autohide>
+                        <Toast.Body>{alertMsg}</Toast.Body>
+                    </Toast>
+                    <div className="collapse navbar-collapse" id="collapsibleNavbar" >
+                        <ul className="navbar-nav ms-auto"  >
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/playlists" >
+                                    Playlists
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <Button variant="clear" className="text-white" onClick={handleLogout}>Logout</Button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                :
+                <div className="container-fluid">
+                    <img src={logo} alt="Generify Music Logo" style={{ width: '50px', height: 'auto', paddingRight: '10px' }} />
+                    <LogoLink className="navbar-brand" to="/" ><b className="main-font">Generify</b></LogoLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Toast className="loginAlert" onClose={() => setShowAlert(false)} variant="success" show={showAlert} delay={2000} autohide>
+                        <Toast.Body>{alertMsg}</Toast.Body>
+                    </Toast>
+                    <div className="collapse navbar-collapse" id="collapsibleNavbar" >
+                        <ul className="navbar-nav ms-auto"  >
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/" >
+                                    Início
+                                </NavLink>
+                            </li>
+                            <li className="nav-item" >
+                                <NavLink className="nav-link" to="/form" >
+                                    Cadastro
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/login" >
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/faq" >
+                                    FAQ
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>}
-            </div>
         </Nav>
     );
 };
